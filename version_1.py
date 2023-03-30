33e7c808191d11bbc10fb5ef8734fb016fed72c1
Lucas Qin <87470079+lucas-project@users.noreply.github.com>
2023-03-30 17:02:03 +1100

    optimized to run super fast, but generate totally black images

--- version_1.py
+++ version_1.py
@@ -23,13 +23,13 @@ def load_images_and_masks(images_path, masks_path):
 
     for file in os.listdir(images_path):
         image = cv2.imread(os.path.join(images_path, file))
-        image = cv2.resize(image, (128, 128))
+        image = cv2.resize(image, (64, 64))
         images.append(image)
 
         mask_path = os.path.join(masks_path, file.replace('.jpg', '.json'))
         with open(mask_path, 'r') as f:
             mask_json = json.load(f)
-        mask = np.zeros((128, 128))
+        mask = np.zeros((64, 64))
         if mask_json['_via_img_metadata'][file + str(os.path.getsize(os.path.join(images_path, file)))]['regions']:
             points = mask_json['_via_img_metadata'][file + str(os.path.getsize(os.path.join(images_path, file)))]['regions'][0]['shape_attributes']['all_points_x_y']
             cv2.fillPoly(mask, np.array([points], dtype=np.int32), 1)
@@ -55,22 +55,22 @@ image_datagen.fit(X_train, augment=True, seed=42)
 mask_datagen.fit(y_train, augment=True, seed=42)
 
 # Build the model (U-Net)
-def build_unet(input_shape=(128, 128, 3)):
+def build_unet(input_shape=(64, 64, 3)):
     inputs = tf.keras.Input(input_shape)
 
-    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(16, (3, 3), activation='relu', padding='same')(inputs)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(32, (3, 3), activation='relu', padding='same')(pool1)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool2)
 
     up4 = Concatenate()([UpSampling2D(size=(2, 2))(conv3), conv2])
-    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(up4)
+    conv4 = Conv2D(32, (3, 3), activation='relu', padding='same')(up4)
 
     up5 = Concatenate()([UpSampling2D(size=(2, 2))(conv4), conv1])
-    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(up5)
+    conv5 = Conv2D(16, (3, 3), activation='relu', padding='same')(up5)
 
     output = Conv2D(1, (1, 1), activation='sigmoid')(conv5)
 
@@ -89,16 +89,18 @@ train_generator = zip(image_datagen.flow(X_train, batch_size=len(X_train), seed=
 val_generator = zip(image_datagen.flow(X_val, batch_size=len(X_val), seed=42),
                     mask_datagen.flow(y_val, batch_size=len(y_val), seed=42))
 
-model.fit(train_generator, validation_data=val_generator, epochs=5)
+model.fit(train_generator, steps_per_epoch=len(X_train) // 2, validation_data=val_generator,
+          validation_steps=len(X_val), epochs=50)
 
 # Evaluate the model's performance on the evaluation set
 def load_evaluation_images(evaluation_path):
     eval_images = []
     for file in os.listdir(evaluation_path):
         image = cv2.imread(os.path.join(evaluation_path, file))
-        image = cv2.resize(image, (128, 128))
+        image = cv2.resize(image, (64, 64))  # Resize the evaluation images to (64, 64)
         eval_images.append(image)
     return np.array(eval_images)
+
 
 evaluation_images = load_evaluation_images(evaluation_path)
 
