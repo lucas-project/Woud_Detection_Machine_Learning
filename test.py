de869768c952ffd5603e22494eb4a5ca3eb6eec4
CiXwOw <cixwow@gmail.com>
2023-05-12 01:49:15 +1000

    Just working on some stuff

--- test.py
+++ test.py
@@ -2,130 +2,123 @@ import os
 import cv2
 
 from datetime import datetime
+from v1_coin import detect_coin
+from v1_measurement import calculate_pixels_per_millimetre_ratio, get_circle_area_px, get_circle_area_mm, get_contour_size_px, get_contour_area_px, get_contour_size_mm, get_contour_area_mm
+from v1_processing import extract_contour_from_outlined_image
+from v1_visualisation import visualise_circle_area_mm, visualise_circle_radius_mm, visualise_circle_diameter_mm, visualise_contour_area_mm, visualise_contour_size_mm
 
-## TODO: UPDATE THESE PATHS
-## TODO: DECIDE WHETHER TO USE ARRAY OF PATHS, OR SINGLE FOLDERS (CURRENT)
-
-#training_image_paths = ['fake_wound/']
-#training_mask_paths = ['fake_jj/']
-training_images_path = 'fake_wound/'
-training_masks_path = 'fake_jj/'
-
-#testing_image_paths = ['fake_evaluation/']
-testing_images_path = 'fake_evaluation/'
+# TODO: Add model paths
+# TODO: Confirm model file name
 
 model_file_name = 'wound_segmentation_model.h5'
 
-COIN_RADIUS = 14.25 # Radius of an Australian $2 coin in millimeters
+COIN_RADIUS_MM = 10.25 # The radius of an Australian $2 coin in millimetres
 
-## MODEL TRAINING
+### PROGRAM TASK FUNCTIONS ###
 
-# TODO: MOVE THIS?
-def dice_coefficient(y_true, y_pred):
-	y_true_f = tf.cast(tf.reshape(y_true, [-1]), tf.float32)
-	y_pred_f = tf.cast(tf.reshape(y_pred, [-1]), tf.float32)
-	intersection = tf.reduce_sum(y_true_f * y_pred_f)
-	return (2. * intersection + 1.) / (tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) + 1.)
-	
-# TODO: MOVE THIS?
-def convert_image_for_display(image):
-		return np.uint8(image[:, :, :3])
-
-def model_train():
-	X, y = load_images_and_masks(images_json_path, images_png_path, masks_json_path, masks_png_path)
-
-	# Data augmentation
-	data_gen_args = dict(rotation_range=20,             # Increase rotation range
-							width_shift_range=0.1,         # Increase width shift range
-							height_shift_range=0.1,        # Increase height shift range
-							shear_range=0.1,               # Increase shear range
-							zoom_range=0.1,                # Increase zoom range
-							brightness_range=(0.9, 1.1),
-							horizontal_flip=True,
-							vertical_flip=True,            # Add vertical flipping
-							fill_mode='nearest')
-	
-	image_datagen = ImageDataGenerator(**data_gen_args)
-	mask_datagen = ImageDataGenerator(**data_gen_args)
-	
-	# Compile the model
-	model.compile(optimizer=Adam(), loss=BinaryCrossentropy(), metrics=[dice_coefficient])
-	
-	# Train the model
-	batch_size = 8  
-
-	train_generator = zip(image_datagen.flow(X_train, batch_size=batch_size, seed=42),
-							mask_datagen.flow(y_train, batch_size=batch_size, seed=42))
-
-	val_generator = zip(image_datagen.flow(X_val, batch_size=batch_size, seed=42),
-							mask_datagen.flow(y_val, batch_size=batch_size, seed=42))
-
-	early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1, restore_best_weights=True)
-
-	model.fit(train_generator, steps_per_epoch=len(X_train) // batch_size, validation_data=val_generator,
-				validation_steps=len(X_val) // batch_size, epochs=1)
-	
-	# Save the model
-	model.save(model_file_name)
-
-## MODEL PREDICTION
-
-def model_predict():
-	# Check to see if the model file exists
-	if os.path.isfile(model_file_name):
-		# Load the model
-		model = load_model(model_file_name)
+def task_wound_analysis_image_processing():
+	while(True):
+		print('------------------------------------------')
+		print('              WOUND ANALYSIS              ')
+		print('             Image Processing             ')
+		print('------------------------------------------')
 		
-		additional_input = True  
-		evaluation_images = load_evaluation_images(evaluation_path, additional_input)
+		# The path of the image for analysis
+		# TODO: Should we change this to accept a user input?
+		input_image_path = 'contour/1.jpg'
 		
-		# Predict and display results
-		predicted_masks = model.predict(evaluation_images)
+		### LOAD/PREPROCESS IMAGES ###
+
+		# Load the image for measurements
+		input_image = cv2.imread(input_image_path)
 		
-		# Set a threshold value
-		threshold = 0.6
-		num_clusters = 8
+		### MEASUREMENTS ###
 		
-		# Apply threshold to the predicted masks
-		binary_masks = (predicted_masks > threshold).astype(np.uint8) * 255
-	
-		return binary_masks
-	else:
-		print(f'ERROR: File "{model_file_name}" not found...')
-		return None
+		## Coin Measurement ##
+		
+		# Get the radius of the found coin in pixels
+		coin_circle, _ = detect_coin(input_image)
+		coin_position_x, coin_position_y, coin_radius_px = coin_circle
+		
+		# Calculate the pixels-per-millimetre ratio
+		pixels_per_millimetre_ratio = calculate_pixels_per_millimetre_ratio(coin_radius_px, COIN_RADIUS_MM)
+		
+		# Get the area of the coin in pixels^2
+		coin_area_px = get_circle_area_px(coin_radius_px)
+		
+		# Convert the coin area to millimetres^2
+		coin_area_mm = get_circle_area_mm(coin_area_px, pixels_per_millimetre_ratio)
+		
+		# Print coin measurements
+		print()
+		print(f'Pixels-per-millimetre Ratio: {pixels_per_millimetre_ratio}')
+		print()
+		print(f'Coin Radius: {"{:.2f}px".format(coin_radius_px)}, {"{:.2f}mm".format(COIN_RADIUS_MM)}')
+		print(f'Coin Area: {"{:.2f}px^2".format(coin_area_px)}, {"{:.2f}mm^2".format(coin_area_mm)}')
+		
+		## Wound Measurement ##
+		
+		# Extract the contour from the input image with a blue outline drawn around the wound
+		wound_contour = extract_contour_from_outlined_image(input_image)
+		
+		# Wound measurements in pixels
+		wound_area_px = get_contour_area_px(wound_contour)
+		wound_length_x_px, wound_length_y_px = get_contour_size_px(wound_contour)
+		
+		# Convert the wound measurements to millimetres
+		wound_area_mm = get_contour_area_mm(wound_contour, pixels_per_millimetre_ratio)
+		wound_length_x_mm, wound_length_y_mm = get_contour_size_mm(wound_contour, pixels_per_millimetre_ratio)
+		
+		# Print wound measurements
+		print()
+		print(f'Wound Length X: {"{:.2f}px".format(wound_length_x_px)}, {"{:.2f}mm".format(wound_length_x_mm)}')
+		print(f'Wound Length Y: {"{:.2f}px".format(wound_length_y_px)}, {"{:.2f}mm".format(wound_length_y_mm)}')
+		print(f'Wound Area: {"{:.2f}px^2".format(wound_area_px)}, {"{:.2f}mm^2".format(wound_area_mm)}')
+		
+		### VISUALISATIONS ###
+		
+		## Visualise Areas ##
+		
+		# Make a copy of the original image
+		image_areas = input_image.copy()
 
-## WOUND MESAUREMENT
+		# Draw coin area visualisation
+		image_areas = visualise_circle_area_mm(image_areas, coin_circle, coin_area_mm)
 
-# TODO: Should we store the mask as a variable when training is complete, or load the mask here as well?
-# TODO: Maybe we could use the mask as well to exclude areas of the image detection?
-def wound_measurement():
-	# Load images (Use this, or adapt existing function to work for both?)
-	image = cv2.imread(testing_images_path)
-	
-	# Find the coin
-	best_circle = detect_coin(image, min_radius=30, max_radius=100)
-	
-	#TODO: return what? Or display results? Save them to file?
+		# Draw wound area visualisation
+		image_areas = visualise_contour_area_mm(image_areas, wound_contour, wound_area_mm)
 
-## COLOUR ANALYSIS
+		## Visualise Lengths ##
 
-# TODO: Should we store the mask as a variable when training is complete, or load the mask here as well?
-def colour_analysis():
-	# Detect percentage of each colour
-	for i, binary_mask in enumerate(binary_masks):
-		original_image = convert_image_for_display(evaluation_images[i])
-		masked_image = extract_color_information(original_image, binary_mask[:, :, 0])
+		# Make a copy of the original image
+		image_lengths = input_image.copy()
 
-		quantized_masked_image, centers = quantize_image(masked_image, num_clusters)
-		color_percentages = calculate_color_percentage(quantized_masked_image, centers)
+		# Draw coin length visualisation
+		# TODO: Current visualisation is diamater. Function also exists to visualise radius. Confirm which is desirable.
+		image_lengths = visualise_circle_diameter_mm(image_lengths, coin_circle, COIN_RADIUS_MM * 2)
 
-		print(f"Color information and their percentage for image {i}:")
-		for color_info in color_percentages:
-			print(f"{color_info[0]} {color_info[1]:.2f}%")
-	
-	#TODO: return what? Or display results? Save them to file?
+		# Draw wound lengths visualisation
+		image_lengths = visualise_contour_size_mm(image_lengths, wound_contour, wound_length_x_mm, wound_length_y_mm)
 
-## TASK SELECTOR
+		## Display Visualisations ##
+
+		# Display visualisations
+		cv2.imshow('Area Measurements', image_areas)
+		cv2.imshow('Length Measurements', image_lengths)
+
+		# Wait for keypress, then close all image windows
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
+		
+		print()
+		print('------------------------------------------')
+		print()
+		print('    PRESS ENTER TO RETURN TO MAIN MENU    ')
+		input()
+		print('------------------------------------------')
+		
+		#cv2.destroyAllWindows()
+		break
 
 # Function for the "About" menu selection.
 # Provides a description of the program and lists the project contributors.
@@ -153,33 +146,28 @@ def task_about():
 	print('------------------------------------------')
 	print()
 	print('    PRESS ENTER TO RETURN TO MAIN MENU    ')
-	
 	input()
-	
 	print('------------------------------------------')
 
-def get_model_creation_date():
-	try:
-		creation_date = os.path.getmtime(model_file_name)
-		return datetime.fromtimestamp(creation_date).strftime('%Y-%m-%d @ %H:%M:%S')
-	except FileNotFoundError:
-		return 'NEVER'
-
-def measure_wound():
-	image = cv2.imread(images_path)
-	detect_coin(image, min_radius=30, max_radius=100)
+### TASK SELECTOR ###
 
 # Array of tasks for main menu
-# [string_title, function_name]
+# [title_string, function_name]
 # When item is selected its function will be called
 tasks = [
+			['Wound Analysis using Image Processing', task_wound_analysis_image_processing],
+			['Wound Analysis using Machine Learning', None],
+			['Train AI Model', None],
 			['About', task_about],
-			['Train Model', None],
-			['Find Wound Area', None],
-			['Measure Wound', None],
-			['Wound Colour Analysis', None],
 			['Quit Program', quit]
 		]
+
+def get_model_model_modified_date():
+	try:
+		model_modified_date = os.path.getmtime(model_file_name)
+		return datetime.fromtimestamp(model_modified_date).strftime('%Y-%m-%d @ %H:%M:%S')
+	except FileNotFoundError:
+		return 'NEVER'
 
 def main():
 	print()
@@ -191,39 +179,53 @@ def main():
 		print()
 		
 		# Gives some information about the mode if one exists
-		print(f'MODEL TRAINED: {get_model_creation_date()}')
+		print(f'MODEL TRAINED: {get_model_model_modified_date()}')
 		print()
 	
 		print('Select a task:')
 		
 		for i, task in enumerate(tasks):
-			print('    ' + str(i) + '. ' + str(task[0]))
+			print('    {0}. {1}'.format(str(i + 1), task[0]))
 		
 		print()
 		print('Please input a number corresponding to one of the tasks...')
 		print('>', end=' ')
 
-		selection = input()
+		selection_str = input()
 		
 		print()
+		#print('------------------------------------------')
+		#print()
 		
-		if selection.isnumeric():
-			selection = int(selection)
+		if selection_str.isnumeric():
+			selection_int = int(selection_str) - 1
 			
-			if selection < len(tasks) and selection >= 0:
-				if tasks[selection][1] is not None:
-					tasks[selection][1]()
+			if selection_int < len(tasks) and selection_int >= 0:
+				if tasks[selection_int][1] is not None:
+					tasks[selection_int][1]()
 					print()
 				else:
-					print('The task "' + str(tasks[selection][0]) + '" has not yet been implemented...')
+					print('------------------------------------------')
 					print()
-					
-			else:
-				print('ERROR: No task corresponding to input: ' + str(selection))
-				print()
+					print('The task "{0}" has not yet been implemented...'.format(tasks[selection_int][0]))
+					print()
+					print('------------------------------------------')
+					print()
 				
+			else:
+				print('------------------------------------------')
+				print()
+				print('ERROR! No task corresponding to input:', selection_str)
+				print()
+				print('------------------------------------------')
+				print()
+			
 		else:
-			print('ERROR: No task corresponding to input: ' + str(selection))
+			print('------------------------------------------')
+			print()
+			print('ERROR! No task corresponding to input:', selection_str)
+			print()
+			print('------------------------------------------')
 			print()
 			
 if __name__ == "__main__":
